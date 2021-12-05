8c4ab8f49fd9786941fd4aa58e00a5e232f25d6a
JettScythe <jettscythe@hotmail.com>
2021-12-05 01:39:37 -0400

    MMMM branding

--- mmmm/pages/index.tsx
+++ mmmm/pages/index.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [log, setLog] = useState<string[]>([])
+  const addToken = (params: any) =>
+    window.ethereum.request({ method: 'wallet_watchAsset', params })
+      .then(() => setLog([...log, 'Success, Token added!']))
+      .catch((error: Error) => setLog([...log, `Error: ${error.message}`]))
+
+  const addMMMMToken = () =>
+  addToken({
+    type: 'ERC20',
+    options: {
+      address: '0x2b591190FF951F60CB9424664155e57A402c1AdE',
+      symbol: 'MMMM',
+      decimals: 3,
+      image: 'https://i.imgur.com/GypGIsZ.jpeg'
+    }
+  })
   return (
     <div className={styles.container}>
       <Head>
@@ -19,9 +35,10 @@ const Home: NextPage = () => {
 
         <p className={styles.description}>
           <code className={styles.code}>CA: 0x2b591190FF951F60CB9424664155e57A402c1AdE</code>
+          <br />
+          <button onClick={addMMMMToken}>Add 🌙🌙🌙🌙</button>
         </p>
-
-        <div>
+        <div className={styles.faq}>
           <h3>FAQ: </h3>
           <p> What is the story about MMMM?  There is only 1000. That is all.</p>
         </div>
